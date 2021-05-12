209decb733873e89c51f6788d39745c014688203
denis salvino <denis.salvino23@gmail.com>
2021-05-12 14:46:52 -0500

    created qr code schema and user schema

---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    Code: require('./codes'),
+    User: require('./user')
+};
---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: 'Name is required'
+    },
+    lastName: {
+        type: String,
+        required: 'Name is required'
+    },
+    username: {
+        type: String,
+        required: 'Username is required',
+        unique: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+    },
+    password: {
+        type: String,
+        len: [8],
+        required: 'Password is required',
+        unique: true
+    }
+});
