670476e98cd48ccb1b33f0fc088f004e54b684ca
denis salvino <denis.salvino23@gmail.com>
2021-05-16 19:53:06 -0500

    added footer i guess

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -3,6 +3,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <>
+            <footer className="footer mt-auto py-3 bg-light">
+                <div className="container text-center">
+                    <span>footer</span>
+                </div>
+            </footer>
         </>
     )
 };
